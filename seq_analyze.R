8b6435aef52b009e3e92c3dc3c2f84793011b953
ggneilc <nchurro25@proton.me>
2024-08-25 16:14:03 -0500

    organized & ppca maybe working

--- seq_analyze.R
+++ seq_analyze.R
@@ -3,54 +3,49 @@ library(pcaMethods)
 library(ggplot2)
 library(data.table)
 
-
-
 # Perform PPCA on DataFrame
-  # Center data 
-  # data.ppca()
-  # graph 
+  # ToDo: Center data 
 analyze_sequences <- function(data_list){
+  result <- ppca(data_list, nPcs=2)         # perform ppca
+  df <- as.data.frame(result@scores)        # turn PCAresponse object into df to graph
 
+  ggplot(df, aes(x = V1, y = V2)) +
+   geom_point() +
+   labs(x = "PC1", y = "PC2", title = "test") +
+  scale_x_continuous(limits = c(-60, 60), breaks = seq(-60, 60, by = 10)) +  # Set x-axis limits and breaks
+   scale_y_continuous(limits = c(-60, 60), breaks = seq(-60, 60, by = 10)) +  # Set y-axis limits and breaks
+   theme_minimal()
 }
 
-get_all_sequences <- function() {
-  unique_seq_ids <- unique(dt$SEQ_ID)
-  sequences <- list()
-  
-  for (seq_id in unique_seq_ids) {
-    sequences[[seq_id]] <- dt[SEQ_ID == seq_id]
-  }
-  
-  return(sequences)
+# Turn the matrix of basecalls into a matrix of sequences
+get_sequences <- function(data){
+  ids <- data$SEQ_ID                                # Extract the SEQ_ID column
+  nids <- length(unique(ids))                       # Count the number of unique IDs
+  tmp <- data[, -1, with = FALSE]                   # remove id column
+  mdata <- as.matrix(tmp)                           # turn datatable into matrix
+  seq_data <- matrix(t(mdata), nrow=nids, byrow=T)  # turn each sequence into a row 
+  return(seq_data)
 }
 
+# Stratified random sampling : group sequences into clusters of `size` and take one sequence from cluster
+subsample <- function(data, size){
+  n_rows <- nrow(data)
+  n_clusters <- ceiling(n_rows / size)
+  clusters <- rep(1:n_clusters, each = size)[1:n_rows]
+  sampled_indices <- tapply(1:n_rows, clusters, function(cluster_rows){
+                              sample(cluster_rows, size=1)
+  })
+  return(data[sampled_indices, ])
+}
+
+start_time <- proc.time()
 # main
 filepath <- "sequence_data.csv"
 data <- fread(filepath)
+seq_data <- get_sequences(data)
+sampled_data <- subsample(seq_data, size = 10)
 gc()
+analyze_sequences(sampled_data)
+end_time <- proc.time()
 
-# Subset the data by sequence
-#seq_12_data <- data[SEQ_ID == "SEQ_1" | SEQ_ID == "SEQ_2"]
-
-# Remove the SEQ_ID column 
-#seq_12_data <- seq_12_data[, -1, with = FALSE]
-#seq_12_data <- as.matrix(seq_12_data)
-ids <- data$SEQ_ID  # Extract the SEQ_ID column
-nids <- length(unique(ids))  # Count the number of unique IDs
-print(nids)  # Print the result
-
-
-tmp <- data[, -1, with = FALSE]
-mdata <- as.matrix(tmp)
-
-mdata <- matrix(t(mdata), nrow=nids, byrow=T)
-gc()
-
-# Perform PPCA
-result <- ppca(mdata, nPcs=2)
-
-print(result@scores)
-# Attempt to retrieve the completed data
-#pdf("Rplots.pdf")
-#plotPcs(result, type="scores")
-#dev.off()
+print(end_time - start_time)
